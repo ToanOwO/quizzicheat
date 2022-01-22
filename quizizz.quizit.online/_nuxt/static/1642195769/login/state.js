6a7f5ceefe51cf0eca0a05333990d49dc51a6ac7
ToanOwO <92969956+ToanOwO@users.noreply.github.com>
2022-01-22 02:56:32 -0500

    Add files via upload

---
+++ quizizz.quizit.online/_nuxt/static/1642195769/login/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return{staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642195769",layout:"navbar",error:a,state:{game:{answers:[],quiz:{},responses:new Map([]),user:a,info:a},user:{user:{},sid:""}},serverRendered:true,routePath:"\u002Flogin",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7d77bfe3"}}}}(null));
